29081835339566d0be0bcd2bc23fb8caa7299585
hastiihp <94233000+hastiihp@users.noreply.github.com>
2025-12-18 14:34:29 +0100

    i fixed some issues

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -6,47 +6,6 @@ The goal of this assignment is to implement by yourself:
   tasks and check that it is working properly.
 - a scikit-learn CV splitter where the splits are based on a Pandas
   DateTimeIndex.
-
-Detailed instructions for question 1:
-The nearest neighbor classifier predicts for a point X_i the target y_k of
-the training sample X_k which is the closest to X_i. We measure proximity with
-the Euclidean distance. The model will be evaluated with the accuracy (average
-number of samples corectly classified). You need to implement the `fit`,
-`predict` and `score` methods for this class. The code you write should pass
-the test we implemented. You can run the tests by calling at the root of the
-repo `pytest test_sklearn_questions.py`. Note that to be fully valid, a
-scikit-learn estimator needs to check that the input given to `fit` and
-`predict` are correct using the `validate_data, check_is_fitted` functions
-imported in this file.
-You can find more information on how they should be used in the following doc:
-https://scikit-learn.org/stable/developers/develop.html#rolling-your-own-estimator.
-Make sure to use them to pass `test_nearest_neighbor_check_estimator`.
-
-
-Detailed instructions for question 2:
-The data to split should contain the index or one column in
-datatime format. Then the aim is to split the data between train and test
-sets when for each pair of successive months, we learn on the first and
-predict of the following. For example if you have data distributed from
-november 2020 to march 2021, you have have 4 splits. The first split
-will allow to learn on november data and predict on december data, the
-second split to learn december and predict on january etc.
-
-We also ask you to respect the pep8 convention: https://pep8.org. This will be
-enforced with `flake8`. You can check that there is no flake8 errors by
-calling `flake8` at the root of the repo.
-
-Finally, you need to write docstrings for the methods you code and for the
-class. The docstring will be checked using `pydocstyle` that you can also
-call at the root of the repo.
-
-Hints
------
-- You can use the function:
-
-from sklearn.metrics.pairwise import pairwise_distances
-
-to compute distances between 2 sets of samples.
 """
 import numpy as np
 import pandas as pd
@@ -79,7 +38,7 @@ class KNearestNeighbors(ClassifierMixin, BaseEstimator):
         Returns
         -------
         self : instance of KNearestNeighbors
-            The current instance of the classifier
+            The current instance of the classifier.
         """
         X, y = validate_data(self, X, y)
         check_classification_targets(y)
@@ -127,12 +86,12 @@ class KNearestNeighbors(ClassifierMixin, BaseEstimator):
         X : ndarray, shape (n_samples, n_features)
             Data to score on.
         y : ndarray, shape (n_samples,)
-            target values.
+            Target values.
 
         Returns
-        ----------
+        -------
         score : float
-        Accuracy of the model computed for the (X, y) pairs.
+            Accuracy of the model computed for the (X, y) pairs.
         """
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
@@ -144,37 +103,28 @@ class MonthlySplit(BaseCrossValidator):
     Split data based on the given `time_col` (or default to index). Each split
     corresponds to one month of data for the training and the next month of
     data for the test.
-
-    Parameters
-    ----------
-    time_col : str, defaults to 'index'
-        Column of the input DataFrame that will be used to split the data. This
-        column should be of type datetime. If split is called with a DataFrame
-        for which this column is not a datetime, it will raise a ValueError.
-        To use the index as column just set `time_col` to `'index'`.
     """
 
     def __init__(self, time_col='index'):  # noqa: D107
         self.time_col = time_col
 
     def get_n_splits(self, X, y=None, groups=None):
-        """Return the number of splitting iterations in the cross-validator.
+        """Return the number of splitting iterations.
 
-       Parameters
-       ----------
-       X : array-like of shape (n_samples, n_features)
-           Training data, where `n_samples` is the number of samples
-           and `n_features` is the number of features.
-       y : array-like of shape (n_samples,)
-           Always ignored, exists for compatibility.
-       groups : array-like of shape (n_samples,)
-           Always ignored, exists for compatibility.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data.
+        y : array-like of shape (n_samples,)
+            Always ignored, exists for compatibility.
+        groups : array-like of shape (n_samples,)
+            Always ignored, exists for compatibility.
 
-       Returns
-       -------
-       n_splits : int
-           The number of splits.
-       """
+        Returns
+        -------
+        n_splits : int
+            The number of splits.
+        """
         if self.time_col == "index":
             dates = pd.Series(X.index)
         else:
@@ -188,22 +138,23 @@ class MonthlySplit(BaseCrossValidator):
 
     def split(self, X, y=None, groups=None):
         """Generate indices to split data into training and test set.
-           Parameters
-            ----------
-            X : array-like of shape (n_samples, n_features)
-                Training data, where `n_samples` is the number of samples
-                and `n_features` is the number of features.
-            y : array-like of shape (n_samples,)
-                Always ignored, exists for compatibility.
-            groups : array-like of shape (n_samples,)
-                Always ignored, exists for compatibility.
-            Yields
-            ------
-            idx_train : ndarray
-                The training set indices for that split.
-            idx_test : ndarray
-                The testing set indices for that split.
-            """
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data.
+        y : array-like of shape (n_samples,)
+            Always ignored, exists for compatibility.
+        groups : array-like of shape (n_samples,)
+            Always ignored, exists for compatibility.
+
+        Yields
+        ------
+        idx_train : ndarray
+            The training set indices for that split.
+        idx_test : ndarray
+            The testing set indices for that split.
+        """
         if not hasattr(X, "index"):
             raise TypeError("X must be a pandas DataFrame")
 
@@ -231,4 +182,5 @@ class MonthlySplit(BaseCrossValidator):
 
             idx_train = sorted_idx[train_mask.values]
             idx_test = sorted_idx[test_mask.values]
+
             yield idx_train, idx_test
